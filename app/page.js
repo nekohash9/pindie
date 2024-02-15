40d1a9e42ee4201c1828555a418fe7003f8f77ce
nekohash9 <nekonotgay@gmail.com>
2024-02-15 20:56:52 +0300

    updates

--- app/page.js
+++ app/page.js
@@ -1,25 +1,21 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
-import { Banner } from "./components/Banner/Banner";
-import { CardsList } from "./components/CardsList/CardsList";
-import { PopularCardsFragment } from "./components/CardsList/PopularCardsFragment";
-import { NewCardsFragment } from "./components/CardsList/NewCardsFragment";
-import { Promo } from "./components/Promo/Promo";
+import { Banner } from './components/Banner/Banner';
+import { CardsList } from './components/CardsList/CardsList';
+import { PopularCardsFragment } from './components/CardsList/PopularCardsFragment';
+import { NewCardsFragment } from './components/CardsList/NewCardsFragment';
+import { Promo } from './components/Promo/Promo';
 
 export default function Home(props) {
   return (
     <main className="main">
       <Banner />
-      <CardsList title={props.id === "popular" ? "Популярные" : "Новинки"}>
-        {props.id === "popular" ? (
-          <PopularCardsFragment />
-        ) : (
-          <NewCardsFragment />
-        )}
+      <CardsList title={props.id === 'popular' ? 'Популярные' : 'Новинки'}>
+        {props.id === 'popular' ? <PopularCardsFragment /> : <NewCardsFragment />}
       </CardsList>
-      <CardsList title={props.id === "new" ? "Новинки" : "Популярные"}>
-        {props.id === "new" ? <NewCardsFragment /> : <PopularCardsFragment />}
+      <CardsList title={props.id === 'new' ? 'Новинки' : 'Популярные'}>
+        {props.id === 'new' ? <NewCardsFragment /> : <PopularCardsFragment />}
       </CardsList>
       <Promo />
     </main>
