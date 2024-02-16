4710e16c9093bfa1c5ec47f798125d374e5cacdd
nekohash9 <nekonotgay@gmail.com>
2024-02-16 23:33:05 +0300

    updates

--- app/components/CardsList/CardsList.jsx
+++ app/components/CardsList/CardsList.jsx
@@ -3,7 +3,6 @@ import { Card } from "../Card/Card";
 import Styles from "./CardsList.module.css";
 
 export const CardsList = (props) => {
-
   return (
     <section className={Styles["list-section"]}>
       <h2 className={Styles["list-section__title"]} id={props.id}>
