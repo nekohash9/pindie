875230aa68605fb62c02a60499a1cf40a0846b94
nekohash9 <nekonotgay@gmail.com>
2024-02-22 21:45:15 +0300

    banner update

--- app/components/Banner/Banner.jsx
+++ app/components/Banner/Banner.jsx
@@ -14,7 +14,7 @@ export const Banner = () => {
             аккаунт пользователя — получаете возможность голосовать за игры.
           </p>
         </div>
-        <a href="#popular" className={`button {Styles.banner__link}`}>
+        <a href="#popular" className={`button ${Styles.banner__link}`}>
           Смотреть игры
         </a>
       </div>
