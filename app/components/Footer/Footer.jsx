786f3e36d5b4b4c15867755acf5ec71b30c2cb5f
nekohash9 <nekonotgay@gmail.com>
2024-02-29 23:06:27 +0300

    footer update

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import Styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
     <footer className={Styles.footer}>
-      <a href="./index.html" className={Styles.footer__logo}>
+      <Link href="/" className={Styles.footer__logo}>
         <span className={Styles["footer__logo-name"]}>pindie</span>
         <span className={Styles["footer__logo-copy"]}>, XXI век</span>
-      </a>
+      </Link>
       <ul className={Styles["social-list"]}>
         <li className={Styles["social-list__item"]}>
           <a href="" className={`button ${Styles["social-list__link"]}`}>
