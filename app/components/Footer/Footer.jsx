94ad8499c3f9403876ce122807e4c811bd43318c
nekohash9 <nekonotgay@gmail.com>
2024-03-02 00:14:20 +0300

    footer.jsx update

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,13 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import Styles from "./Footer.module.css";
+import { usePathname } from "next/navigation";
 
 export const Footer = () => {
+  const pathname = usePathname();
   return (
     <footer className={Styles.footer}>
-      <Link href="/" className={Styles.footer__logo}>
-        <span className={Styles["footer__logo-name"]}>pindie</span>
-        <span className={Styles["footer__logo-copy"]}>, XXI век</span>
-      </Link>
+      {pathname === "/" ? (
+        <div className={Styles.footer__logo}>
+          <span className={Styles["footer__logo-name"]}>pindie</span>
+          <span className={Styles["footer__logo-copy"]}>, XXI век</span>
+        </div>
+      ) : (
+        <Link href="/" className={Styles.footer__logo}>
+          <span className={Styles["footer__logo-name"]}>pindie</span>
+          <span className={Styles["footer__logo-copy"]}>, XXI век</span>
+        </Link>
+      )}
       <ul className={Styles["social-list"]}>
         <li className={Styles["social-list__item"]}>
           <a href="" className={`button ${Styles["social-list__link"]}`}>
