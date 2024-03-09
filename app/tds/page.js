cb0d26ef9bae0cc26e0d287be02b115e4d1b8311
nekohash9 <nekonotgay@gmail.com>
2024-03-09 13:50:18 +0300

    tds preloader and api request

--- app/tds/page.js
+++ app/tds/page.js
@@ -1,8 +1,9 @@
 import { getGamesByCategory } from "../data/data-utils";
 import { CardsList } from "../components/CardsList/CardsList";
+import { endpoints } from "../api/config";
 
-export default function Tds() {
-  const TDS = getGamesByCategory("TDS");
+export default async function Tds() {
+  const TDS = await getGamesByCategory(endpoints.games, "TDS");
   return (
     <main className="main-inner">
       <CardsList id="TDS" title="TDS" data={TDS} />
