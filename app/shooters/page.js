eaa23a1fe235692e6115f38e146ff6e2a7e9de71
nekohash9 <nekonotgay@gmail.com>
2024-03-09 13:52:57 +0300

    shooters page api request initial and preloader

--- app/shooters/page.js
+++ app/shooters/page.js
@@ -1,8 +1,9 @@
 import { getGamesByCategory } from "../data/data-utils";
 import { CardsList } from "../components/CardsList/CardsList";
+import { endpoints } from "../api/config";
 
-export default function Shooters() {
-  const shooter = getGamesByCategory("shooter");
+export default async function Shooters() {
+  const shooter = await getGamesByCategory(endpoints.games, "shooter");
   return (
     <main className="main-inner">
       <CardsList id="shooter" title="Шутеры" data={shooter} />
