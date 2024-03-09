de3be99f0984b18faa7395552ecaa8de29614c19
nekohash9 <nekonotgay@gmail.com>
2024-03-09 13:48:02 +0300

    api initial

---
+++ app/api/api-utils.js
@@ -0,0 +1,78 @@
+export const isResponseOk = (response) => {
+  return !(response instanceof Error);
+};
+
+export const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const normalizeDataObject = (obj) => {
+  return {
+    ...obj,
+    category: obj.categories,
+    users: obj.users_permissions_users,
+  };
+};
+
+export const normalizeData = (data) => {
+  return data.map((item) => {
+    return normalizeDataObject(item);
+  });
+};
+
+export const getNormalizedGamesDataByCategory = async (url, category) => {
+  const data = await getData(`${url}?categories.name=${category}`);
+  return isResponseOk(data) ? normalizeData(data) : data;
+};
+
+export const getNormalizedGameDataById = async (url, id) => {
+  const data = await getData(`${url}/${id}`);
+  return isResponseOk(data) ? normalizeDataObject(data) : data;
+};
+
+export const authorize = async (url, data) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (response.status !== 200) {
+      throw new Error("Ошибка авторизации");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getMe = async (url, jwt) => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+    if (response.status !== 200) {
+      throw new Error("Ошибка получения данных");
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const setJWT = (jwt) => {
+  localStorage.setItem("jwt", jwt);
+};
+
+export const getJWT = (jwt) => {
+  return localStorage.getItem("jwt");
+};
+
+export const removeJWT = (jwt) => {
+  localStorage.removeItem("jwt");
+};
