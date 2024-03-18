47a2ef07c8d9320bbb38f9f4152f409380075dbc
nekohash9 <nekonotgay@gmail.com>
2024-03-18 20:50:02 +0300

    add: global state for main page

--- app/page.js
+++ app/page.js
@@ -1,27 +1,37 @@
-import { getGamesByCategory } from "./data/data-utils";
+"use client";
+
+import { endpoints } from "./api/config";
 import { Banner } from "./Components/Banner/Banner";
 import { CardsListSection } from "./Components/CardsListSection/CardsListSection";
 import { Promo } from "./Components/Promo/Promo";
+import { useGetDataByCategory } from "./api/api-hooks";
+import { Preloader } from "./Components/Preloader/Preloader";
 
 export default function Home() {
-  const popularGames = getGamesByCategory("popular");
-  const newGames = getGamesByCategory("new");
+  const popularGames = useGetDataByCategory(endpoints.games, "popular");
+  const newGames = useGetDataByCategory(endpoints.games, "new");
   return (
     <main>
-      <Banner />
-      <CardsListSection
-        type="slider"
-        id="popular"
-        title="Популярные"
-        data={popularGames}
-      />
-      <CardsListSection
-        type="slider"
-        id="new"
-        title="Новинки"
-        data={newGames}
-      />
-      <Promo />
+      {popularGames && newGames ? (
+        <>
+          <Banner />
+          <CardsListSection
+            type="slider"
+            id="popular"
+            title="Популярные"
+            data={popularGames}
+          />
+          <CardsListSection
+            type="slider"
+            id="new"
+            title="Новинки"
+            data={newGames}
+          />
+          <Promo />
+        </>
+      ) : (
+        <Preloader />
+      )}
     </main>
   );
 }
