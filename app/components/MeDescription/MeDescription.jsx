4175d31da345bbeefcf3f99d8639fa5d6d69d285
nekohash9 <nekonotgay@gmail.com>
2024-03-26 21:05:53 +0300

    update: MeDescription.jsx

--- app/components/MeDescription/MeDescription.jsx
+++ app/components/MeDescription/MeDescription.jsx
@@ -1,47 +1,39 @@
 "use client";
 
 import Styles from "./MeDescription.module.css";
-import { useStore } from "@/app/store/app-store";
 import { useRouter } from "next/navigation";
-
-// import { authorize } from "@/app/api/api-utils";
-// import { endpoints } from "@/app/api/config";
-// import { useEffect } from "react";
+import { useStore } from "@/app/store/app-store";
+import Preloader from "../Preloader/Preloader";
 
 export const MeDescription = () => {
   const authContext = useStore();
   const router = useRouter();
+  const store = useStore();
 
   const handleLogout = () => {
     router.push("/");
     authContext.logout();
   };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await authorize(endpoints.auth, authData);
-  //       const data = await response.json();
-  //       setResult(data);
-  //     } catch (error) {
-  //       console.error("Error fetching data:", error);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   return (
     <section className={Styles["about__page_user"]}>
       <h2 className={Styles["about__page_title"]}>Страница аккаунта</h2>
-      <div className={Styles["about__page_field"]}>
-        <h4 className={Styles["about__page_desc"]}>Имя пользователя:</h4>
-        <p className={Styles["about__page_answer"]}>Имя</p>
-      </div>
-      <div className={Styles["about__page_field"]}>
-        <h4 className={Styles["about__page_desc"]}>Email: </h4>
-        <p className={Styles["about__page_answer"]}>Email</p>
-      </div>
+      {store.user ? (
+        <>
+          <div className={Styles["about__page_field"]}>
+            <h4 className={Styles["about__page_desc"]}>Юзернэйм: </h4>
+            <p className={Styles["about__page_answer"]}>
+              {store.user.username}
+            </p>
+          </div>
+          <div className={Styles["about__page_field"]}>
+            <h4 className={Styles["about__page_desc"]}>Email: </h4>
+            <p className={Styles["about__page_answer"]}>{store.user.email}</p>
+          </div>
+        </>
+      ) : (
+        <Preloader />
+      )}
       <button
         onClick={handleLogout}
         className={`button ${Styles["about__exit-button"]}`}
